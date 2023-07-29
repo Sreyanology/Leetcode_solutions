f1782684b754e1ef41aa1d8c2cfe83dac67a66b8
Sreyanology <93214021+Sreyanology@users.noreply.github.com>
2023-07-29 15:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (42.84%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    int f(vector<int>& nums, int i, vector<int>& dp)
+    {
+        if(i>=nums.size())
+         return 0;
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = max(nums[i] + f(nums, i+2, dp), f(nums, i+1, dp));
+    }
+public:
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(), -1);
+        return f(nums, 0, dp);
+    }
+};
