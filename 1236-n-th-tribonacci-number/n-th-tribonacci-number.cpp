629e60e9fe37e210067be8cf14d41e9c5800176b
Sreyanology <93214021+Sreyanology@users.noreply.github.com>
2023-07-29 14:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (19.57%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int f(int n, vector<int>& dp)
+    {
+        if(n<=2 && n>0)
+         return 1;
+        else if(n==0)
+         return 0;
+        else if(n<0)
+         return 0;
+        
+        if(dp[n]!= -1) return dp[n];
+        
+        return dp[n] = f(n-1, dp) + f(n-2, dp) + f(n-3, dp);
+    }
+    int tribonacci(int n) {
+        vector<int>dp(n+1, -1);
+        return f(n, dp);
+    }
+};
